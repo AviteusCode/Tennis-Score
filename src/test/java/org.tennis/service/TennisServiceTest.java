30d9c7e497305b9b8d5801a819c3c2c103bfc00d
Aviteus <kevin.avi.legay@gmail.com>
2025-07-15 12:11:14 +0200

    init test

---
+++ src/test/java/org.tennis/service/TennisServiceTest.java
@@ -0,0 +1,71 @@
+package org.tennis.service;
+
+import org.junit.jupiter.api.Test;
+
+public class TennisServiceTest {
+
+    @Test
+    public void shouldReturnScore(){
+
+    }
+
+    @Test
+    public void shouldReturnScore2(){
+
+    }
+
+    @Test
+    public void shouldReturnDeuce(){
+
+    }
+
+    @Test
+    public void shouldReturnAdvantage(){
+
+    }
+
+    @Test
+    public void shouldUndoAdvantage(){
+
+    }
+
+    @Test
+    public void shouldRedoAdvantage(){
+
+    }
+
+    @Test
+    public void shouldDoOppositeAdvantage(){
+
+    }
+
+    @Test
+    public void shouldReturnBWinner(){
+
+    }
+
+    @Test
+    public void shouldReturnAWinnerAfterAdvantage(){
+
+    }
+
+    @Test
+    public void shouldReturnAnErrorIfEntryIsNull(){
+
+    }
+
+    @Test
+    public void shouldReturnAnErrorIfEntryIsEmpty(){
+
+    }
+
+    @Test
+    public void shouldStopAndPrintAnErrorAfterVictory(){
+
+    }
+
+    @Test
+    public void shouldNotStartIfErrorInEntry(){
+    }
+    
+}
